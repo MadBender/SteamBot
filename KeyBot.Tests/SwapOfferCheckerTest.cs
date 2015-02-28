d8b80722f062d6e5057ea89282e92c40951757f8
Artem vasilyev <ryder86@gmail.com>
2015-02-28 19:55:45 +0300

    swap offer checker

---
+++ KeyBot.Tests/SwapOfferCheckerTest.cs
@@ -0,0 +1,172 @@
+﻿using System;
+using System.Collections.Generic;
+using KeyBot.Models;
+using KeyBot.OfferCheckers;
+using Microsoft.VisualStudio.TestTools.UnitTesting;
+using SteamTrade.TradeOffer;
+
+namespace KeyBot.Tests
+{
+    [TestClass]
+    public class SwapOfferCheckerTest
+    {
+        private OfferChecker Checker;
+
+        //sample objects
+
+        private static CEconAssetModel CsGoKey = new CEconAssetModel {
+            AppId = "730",
+            InstanceId = "143865972",
+            ClassId = "186150629",
+            Description = new AssetDescription {
+                MarketHashName = "CS:GO Case Key"
+            },
+            Price = 2.49m
+        };
+
+        private static CEconAssetModel BreakoutKey = new CEconAssetModel {
+            AppId = "730",
+            InstanceId = "143865972",
+            ClassId = "613589848",
+            Description = new AssetDescription {
+                MarketHashName = "Operation Breakout Case Key"
+            },
+            Price = 2.49m
+        };        
+
+        private static HashSet<string> KeyNames = new HashSet<string>{
+            "Operation Phoenix Case Key",
+            "CS:GO Case Key",
+            "Operation Breakout Case Key",
+            "Huntsman Case Key",
+            "eSports Key",
+            "Winter Offensive Case Key",
+            "Operation Vanguard Case Key",
+            "Chroma Case Key"
+        };
+
+        public SwapOfferCheckerTest()
+        {
+            var freeKeys = new HashSet<string>{
+                "CS:GO Case Key",
+                "Winter Offensive Case Key",
+                "Chroma Case Key"
+            };
+
+            Checker = new SwapOfferChecker(
+                (my, their) => {
+                    if (IsKey(my) && IsKey(their)) {
+                        return freeKeys.Contains(my.Description.MarketHashName) && !freeKeys.Contains(their.Description.MarketHashName)
+                            ? 0m
+                            : 0.05m;
+                    }
+                    return null;
+                });            
+        }
+
+        private bool IsKey(CEconAssetModel asset)
+        {
+            return asset.InstanceId == "143865972"
+                && asset.AppId == "730"
+                && asset.Description != null
+                && KeyNames.Contains(asset.Description.MarketHashName);
+        }
+
+        private CEconAssetModel Item(decimal? price)
+        {
+            return new CEconAssetModel { Price = price };
+        }
+
+        [TestMethod]
+        public void CorrectOffer()
+        {
+            Assert.IsTrue(Checker.CheckOffer(new OfferModel {
+                ItemsToGive = new List<CEconAssetModel>{ BreakoutKey },
+                ItemsToReceive = new List<CEconAssetModel>{ CsGoKey, Item(0.05m) }
+            }));
+        }
+
+        [TestMethod]
+        public void LowAdds()
+        {
+            Assert.IsFalse(Checker.CheckOffer(new OfferModel {
+                ItemsToGive = new List<CEconAssetModel> { BreakoutKey },
+                ItemsToReceive = new List<CEconAssetModel> { CsGoKey, Item(0.04m) }
+            }));
+        }
+
+        [TestMethod]
+        public void MultipleAdds()
+        {
+            Assert.IsTrue(Checker.CheckOffer(new OfferModel {
+                ItemsToGive = new List<CEconAssetModel> { BreakoutKey },
+                ItemsToReceive = new List<CEconAssetModel> { CsGoKey, Item(0.04m), Item(0.04m) }
+            }));
+        }
+
+        [TestMethod]
+        public void NoAdds()
+        {
+            Assert.IsFalse(Checker.CheckOffer(new OfferModel {
+                ItemsToGive = new List<CEconAssetModel> { BreakoutKey },
+                ItemsToReceive = new List<CEconAssetModel> { CsGoKey }
+            }));
+        }
+
+        [TestMethod]
+        public void FreeSwap()
+        {
+            Assert.IsTrue(Checker.CheckOffer(new OfferModel {
+                ItemsToGive = new List<CEconAssetModel> { CsGoKey },
+                ItemsToReceive = new List<CEconAssetModel> { BreakoutKey }
+            }));
+        }
+
+        [TestMethod]
+        public void WantsMyItems()
+        {
+            Assert.IsFalse(Checker.CheckOffer(new OfferModel {
+                ItemsToGive = new List<CEconAssetModel> { CsGoKey, Item(0.05m) },
+                ItemsToReceive = new List<CEconAssetModel> { BreakoutKey }
+            }));
+        }
+
+        [TestMethod]
+        public void DonatesItems()
+        {
+            Assert.IsTrue(Checker.CheckOffer(new OfferModel {
+                ItemsToGive = new List<CEconAssetModel> {  },
+                ItemsToReceive = new List<CEconAssetModel> { Item(0.05m) }
+            }));
+        }
+
+        [TestMethod]
+        public void MixedSwap()
+        {
+            Assert.IsTrue(Checker.CheckOffer(new OfferModel {
+                ItemsToGive = new List<CEconAssetModel> { CsGoKey, BreakoutKey },
+                ItemsToReceive = new List<CEconAssetModel> { BreakoutKey, BreakoutKey, Item(0.05m) }
+            }));
+        }
+
+        [TestMethod]
+        public void NullPrice()
+        {
+            Assert.IsTrue(Checker.CheckOffer(new OfferModel {
+                ItemsToGive = new List<CEconAssetModel> { BreakoutKey },
+                ItemsToReceive = new List<CEconAssetModel> { CsGoKey, Item(null), Item(0.05m) }
+            }));
+        }
+
+        [TestMethod]
+        public void ExtraKeys()
+        {
+            Assert.IsTrue(Checker.CheckOffer(new OfferModel {
+                ItemsToGive = new List<CEconAssetModel> { BreakoutKey },
+                ItemsToReceive = new List<CEconAssetModel> { CsGoKey, CsGoKey }
+            }));
+        }
+
+
+    }
+}
---
+++ KeyBot/OfferCheckers/SwapOfferChecker.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using KeyBot.Models;
+
+namespace KeyBot.OfferCheckers
+{
+    public class SwapOfferChecker: OfferChecker
+    {        
+        //parameters: my item, their item, swap price (null if can't swap)
+        private Func<CEconAssetModel, CEconAssetModel, decimal?> SwapFunc;
+
+        public SwapOfferChecker(Func<CEconAssetModel, CEconAssetModel, decimal?> swapFunc)
+        {
+            SwapFunc = swapFunc;
+        }
+
+        public override bool CheckOffer(OfferModel o)
+        {
+            var theirItems = new List<CEconAssetModel>(o.ItemsToReceive);
+            decimal totalSwapPrice = 0;
+
+            //searhing a cheapest swap pair for every item of mine
+            foreach (CEconAssetModel myItem in o.ItemsToGive) {
+                CEconAssetModel swapItem = null;
+                decimal itemSwapPrice = decimal.MaxValue;
+                //looking for pair
+                foreach(CEconAssetModel theirItem in theirItems) {
+                    decimal? sPrice = SwapFunc(myItem, theirItem);
+                    if (sPrice != null && sPrice.Value < itemSwapPrice) {
+                        swapItem = theirItem;
+                        itemSwapPrice = sPrice.Value;
+                        if (itemSwapPrice == 0) {
+                            //free swap - no point to search further
+                            break;
+                        }
+                    }
+                }
+
+                if (swapItem == null) {
+                    //a pair for my item was not found - don't accept this offer
+                    return false;
+                }
+
+                theirItems.Remove(swapItem);
+                totalSwapPrice += itemSwapPrice;
+            }
+
+            //what left in theirItems is swap fee
+            decimal addPrice = theirItems.Sum(it => it.Price != null ? it.Price.Value : 0);
+            return addPrice >= totalSwapPrice;
+        }
+    }
+}
